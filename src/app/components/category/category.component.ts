b7db4f76f8b018a5759d6be95601364d54b0a1f0
ASHOK <ashoktut.2@gmail.com>
2022-02-05 04:11:46 +0200

    category component

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -8,10 +8,95 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   // initializing empty object of category
-  @Input() category: any;
+  @Input() categories: any;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.categories = [ // category object and its attributes
+      {
+          uid: 'omburo',
+          cover: 'assets/images/categories/vegan.jpg',
+          name: 'Vegan',
+          shortName: 'veg',
+          cuisines: [ // cusines array
+            'Vegan'
+          ],
+          rating: 5,
+          deliveryTime: 25,
+          distance: 2.5,
+          price: 100
+      },
+      {
+        uid: 'sdu8fdf',
+        cover: 'assets/images/categories/chicken1.jpg',
+        name: 'Chicken',
+        shortName: 'chi',
+        cuisines: [ // cusines array
+          'Chicken'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 100
+      },
+
+      {
+        uid: 'jfl8f',
+        cover: 'assets/images/categories/beef2.jpg',
+        name: 'Beef',
+        shortName: 'beef',
+        cuisines: [ // cusines array
+          'Beef'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 100
+      },
+
+      {
+        uid: 'nkojoro',
+        cover: 'assets/images/categories/snacks3.jpg',
+        name: 'Snacks',
+        shortName: 'snacks',
+        cuisines: [ // cusines array
+          'Snacks'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 100
+      },
+
+      {
+        uid: 'nwefvi',
+        cover: 'assets/images/categories/extras2.jpg',
+        name: 'Extras',
+        shortName: 'extras',
+        cuisines: [ // cusines array
+          'Extras'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 100
+      },
+
+      {
+        uid: 'ujala',
+        cover: 'assets/images/categories/drinks1.jpg',
+        name: 'Drinks',
+        shortName: 'drinks',
+        cuisines: [ // cusines array
+          'Beverages'
+        ],
+        rating: 5,
+        deliveryTime: 25,
+        distance: 2.5,
+        price: 100
+      },
+    ];
+  }
 
   // function to join array and converting it to a string
   getCuisine(cuisine) {
